d250bbcda777c22f3834d8b3648e81adcc34b8f5
petchkubb <petchkubb@live.com>
2019-04-17 10:45:48 +0700

    add back button

--- src/screen/Restaurant.js
+++ src/screen/Restaurant.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ScrollView, Animated,Image } from 'react-native'
-import {Tabs,Tab,Container, Content,Card,CardItem} from 'native-base'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Animated,
+  Image
+} from 'react-native'
+import {
+  Tabs,
+  Tab,
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Icon
+} from 'native-base'
+import { Actions } from 'react-native-router-flux';
 
 const HEADER_MAX_HEIGHT = 200
 const HEADER_MIN_HEIGHT = 60
@@ -11,21 +27,19 @@ export default class Restaurant extends Component {
     super(props)
     this.state = {
       scrollY: new Animated.Value(0),
-      filterMenu:[]
-
+      filterMenu: []
     }
   }
-componentDidMount(){
-    const initfilter = this.props.item.Menu.filter((item)=>{
+  componentDidMount () {
+    const initfilter = this.props.item.Menu.filter(item => {
       return item.type == this.props.item.type[0]
     })
-     this.setState({filterMenu:initfilter})
-}
+    this.setState({ filterMenu: initfilter })
+  }
   _renderScrollViewContent () {
-  
     const data = Array.from({ length: 30 })
     return (
-      <View >
+      <View>
         {data.map((_, i) => (
           <View key={i} style={styles.row}>
             <Text>{i}</Text>
@@ -35,56 +49,55 @@ componentDidMount(){
     )
   }
 
-  filterMenu(i) {
-   const data =  this.props.item.Menu.filter((item)=>{
+  filterMenu (i) {
+    const data = this.props.item.Menu.filter(item => {
       return item.type == i.ref.props.heading
     })
-    this.setState({filterMenu:data})
+    this.setState({ filterMenu: data })
   }
 
-  tabBar = () =>{
-    return(
-       <Container>
-        <Tabs  onChangeTab={(i,ref,from)=> this.filterMenu(i)}>
-        {this.props.item.type.map((data,index)=>(
-          <Tab heading={data} key={data} >
-         
-        <ScrollView
-          style={styles.fill}
-          scrollEventThrottle={4}
-          onScroll={Animated.event([
-            { nativeEvent: { contentOffset: { y: this.state.scrollY } } }
-          ])}
-        >
-              {this.state.filterMenu.map((item,index)=>{
-                return(
-                
-                  <Card key={index}>
-          <CardItem cardBody>
-            <Image source={{uri:item.img}} style={{width:'100%',height:150}}/>
-          </CardItem>
-          <CardItem >
-              <Text>{item.name}</Text>
-          </CardItem>
-        </Card>
-        
-                )
-              })}
-              {this._renderScrollViewContent()}
-              </ScrollView>
-          </Tab>
-         ))}
-         </Tabs>
+  tabBar = () => {
+    return (
+      <Container>
+        <Tabs onChangeTab={(i, ref, from) => this.filterMenu(i)}>
+          {this.props.item.type.map((data, index) => (
+            <Tab heading={data} key={data}>
+              <Animated.ScrollView
+                style={styles.fill}
+                onScroll={Animated.event([
+                  { nativeEvent: { contentOffset: { y: this.state.scrollY } } }
+                ])}
+              >
+                {this.state.filterMenu.map((item, index) => {
+                  return (
+                    <Card key={index}>
+                      <CardItem cardBody>
+                        <Image
+                          source={{ uri: item.img }}
+                          style={{ width: '100%', height: 150 }}
+                        />
+                      </CardItem>
+                      <CardItem>
+                        <Text>{item.name}</Text>
+                      </CardItem>
+                    </Card>
+                  )
+                })}
+                {this._renderScrollViewContent()}
+              </Animated.ScrollView>
+            </Tab>
+          ))}
+        </Tabs>
       </Container>
     )
   }
 
   render () {
-    
-   const tabY = this.state.scrollY.interpolate({
-      inputRange: [0, HEADER_SCROLL_DISTANCE, HEADER_SCROLL_DISTANCE+ 1],
-      outputRange: [0, 0, 1]
-    });
+    const tabY = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE, HEADER_SCROLL_DISTANCE + 1],
+      outputRange: [0, 0, 1],
+      extrapolate: 'clamp'
+    })
 
     const tabHeight = this.state.scrollY.interpolate({
       inputRange: [0, HEADER_SCROLL_DISTANCE],
@@ -111,11 +124,10 @@ componentDidMount(){
       outputRange: [1, 1, 0],
       extrapolate: 'clamp'
     })
-    const { Name,Img} = this.props.item
-    
+    const { Name, Img } = this.props.item
+
     return (
       <View style={styles.container}>
-        
         <Animated.View style={[styles.header, { height: headerHeight }]}>
           <Animated.Image
             style={[
@@ -125,40 +137,25 @@ componentDidMount(){
                 transform: [{ translateY: imageTranslate }]
               }
             ]}
-            source={{uri:Img}}
+            source={{ uri: Img }}
           />
           <Animated.View style={styles.bar}>
+            <Icon
+              name='arrow-back'
+              type='MaterialIcons'
+              style={{
+                color: 'white',
+                fontSize: 24,
+                marginRight: 110,
+              }}
+              onPress={()=>Actions.pop()}
+            />
             <Text style={styles.title}>{Name}</Text>
           </Animated.View>
         </Animated.View>
-        <Animated.View style={[styles.tab, {top: tabHeight }]}> 
-        
-       
+        <Animated.View style={[styles.tab, { marginTop: tabHeight }]}>
           {this.tabBar()}
-          
-       
-       {/* <Container>
-        <Tabs >
-          
-          <Tab heading="1">
-       
-          <ScrollView
-          style={styles.fill}
-          scrollEventThrottle={4}
-          onScroll={Animated.event([
-            { nativeEvent: { contentOffset: { y: this.state.scrollY } } }
-          ])}
-        >
-          {this._renderScrollViewContent()}
-        </ScrollView>   
-          
-          </Tab>
-        
-          <Tab heading="2"></Tab>
-        </Tabs>
-        </Container> */}
         </Animated.View>
-       
       </View>
     )
   }
@@ -170,7 +167,8 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   fill: {
-    flex: 1
+    flex: 1,
+    marginTop: 5
   },
   row: {
     height: 40,
@@ -186,33 +184,35 @@ const styles = StyleSheet.create({
     right: 0,
     backgroundColor: '#03A9F4',
     overflow: 'hidden',
-    height: HEADER_MAX_HEIGHT,
+    height: HEADER_MAX_HEIGHT
   },
-  tab:{
+  tab: {
     position: 'absolute',
     left: 0,
     right: 0,
-    overflow: 'hidden',
-
+    overflow: 'hidden'
   },
   bar: {
+    
+    flexDirection: 'row',
     backgroundColor: 'transparent',
     marginTop: 20,
+    marginLeft: 5,
     height: 32,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'flex-start',
     position: 'absolute',
     top: 0,
     left: 0,
-    right: 0,
+    right: 0
   },
   title: {
     backgroundColor: 'transparent',
     color: 'white',
-    fontSize: 18
+    fontSize: 18,
+    
   },
   scrollViewContent: {
-    marginTop: HEADER_MAX_HEIGHT+60
+    marginTop: HEADER_MAX_HEIGHT + 60
   },
   backgroundImage: {
     position: 'absolute',
