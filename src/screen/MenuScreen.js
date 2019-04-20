a4c8f7046ef1eb4304b767038104eb43bee42135
petchkubb <petchkubb@live.com>
2019-04-20 16:24:58 +0700

    add radio button

--- src/screen/MenuScreen.js
+++ src/screen/MenuScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Animated,
   Platform,
@@ -8,36 +8,44 @@ import {
   View,
   Image,
   RefreshControl,
-  TouchableOpacity,Dimensions,CheckBox
-} from 'react-native';
-import {
-  
-  Card,
-  CardItem,
-  Icon,
-  Left,
-  Right
-} from 'native-base'
-import { Actions } from 'react-native-router-flux';
-import ParallaxScrollView from 'react-native-parallax-scroll-view';
-import RadioGroup from 'react-native-radio-buttons-group';
+  TouchableOpacity,
+  Dimensions,
+  CheckBox
+} from 'react-native'
+import { Card, CardItem, Icon, Left, Right } from 'native-base'
+import { Actions } from 'react-native-router-flux'
+import ParallaxScrollView from 'react-native-parallax-scroll-view'
+import RadioGroup from 'react-native-radio-button-group'
+
+
 
 const PARALLAX_HEADER_HEIGHT = 300
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      num:0
-    };
+      num: 0,
+      selectedOption: null
+    }
   }
 
-  _renderScrollViewContent(menu) {
-    
+  _renderScrollViewContent (menu) {
+    var select = [];
+    menu.customize.choice.forEach((item,index)=>{
+      select.push({
+        id:index , 
+        labelView: (
+        <Text key={index}>
+           {item.item}
+        </Text>
+      )},
+      )
+    })
+   
     return (
       <View style={styles.scrollViewContent}>
-      
-      <Card>
+        <Card>
           <CardItem>
             <Left>
               <Text>{menu.name}</Text>
@@ -50,99 +58,108 @@ export default class App extends Component {
         <View style={styles.customize}>
           <Text>Customize</Text>
           <Text>{menu.customize.question} (Choose One)</Text>
-          {menu.customize.choice.map((item,index)=>{
+          <View>
+          <RadioGroup
+            options={select}
             
-            return(
-              <View style={{flexDirection:'row'}} key={index}>
-              
-              <Text key={index}>{item.item}</Text>
-              </View>
-            )
-          })}
+            onChange={(option) => this.setState({selectedOption: option})}
+          />
+          </View>
         </View>
       </View>
-    );
+    )
   }
 
-  render() {
-    
-    const {img,name,price} = this.props.menu
-    const { num } = this.state; 
+  render () {
+    const { img, name, price } = this.props.menu
+    const { num } = this.state
     return (
       <View style={styles.container}>
         <ParallaxScrollView
-      stickyHeaderHeight={ 80 }
-      backgroundColor="blue"
-      contentBackgroundColor="white"
-      parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
-      fadeOutForeground={true}
-      renderStickyHeader={()=> (
-      <View key="sticky-header"  style={{ height: 200, justifyContent: 'flex-start',top:20}}>
-      <Icon
-              name='close'
-              type='MaterialIcons'
+          stickyHeaderHeight={80}
+          backgroundColor='blue'
+          contentBackgroundColor='white'
+          parallaxHeaderHeight={PARALLAX_HEADER_HEIGHT}
+          fadeOutForeground
+          renderStickyHeader={() => (
+            <View
+              key='sticky-header'
+              style={{ height: 200, justifyContent: 'flex-start', top: 20 }}
+            >
+              <Icon
+                name='close'
+                type='MaterialIcons'
+                style={{
+                  color: 'white',
+                  fontSize: 32,
+                  marginRight: 5,
+                  marginTop: 22
+                }}
+                onPress={() => {
+                  Actions.pop()
+                  setTimeout(() => {
+                    Actions.refresh({
+                      param: '555'
+                    })
+                  }, 0)
+                }}
+              />
+            </View>
+          )}
+          renderForeground={() => (
+            <View
               style={{
-                color: 'white',
-                fontSize: 32,
-                marginRight: 5,
-                marginTop: 22,
+                height: 200,
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
               }}
-              onPress={()=>{Actions.pop()
-                setTimeout(() => {
-                  Actions.refresh({
-                  param : "555"
-                  });
-                  }, 0); 
-              }
-              }
             />
-      </View>
-  )}
-      renderForeground={() => (
-       <View style={{ height: 200, flex: 1, alignItems: 'center', justifyContent: 'center' ,}}>
-          
-        </View>
-      )}
-      renderBackground={() => (
-        <View key="background" style={{backgroundColor:'white'}}>
-          <Image source={{uri: img,
-                          width: '100%',
-                          height: PARALLAX_HEADER_HEIGHT}} style={{marginTop:20}}/>
-          
-        </View>
-      )}
-      
-      >
-      <View style={{ height: 500 }}>
-      {this._renderScrollViewContent(this.props.menu)}
-      </View>
-
-     
-    </ParallaxScrollView>
-    <View
-      style={{
-        backgroundColor: 'white',
-        height: 60,
-        justifyContent:'space-around',
-        borderColor:"black",
-        borderWidth:1,
-        padding:5,
-        flexDirection: 'row'
-      }}
-    >
-    <View style={{
-    textAlign: 'center',
-    justifyContent:'center',
-    flexDirection: 'row',
-    width:150}}>
-    <Icon
+          )}
+          renderBackground={() => (
+            <View key='background' style={{ backgroundColor: 'white' }}>
+              <Image
+                source={{
+                  uri: img,
+                  width: '100%',
+                  height: PARALLAX_HEADER_HEIGHT
+                }}
+                style={{ marginTop: 20 }}
+              />
+            </View>
+          )}
+        >
+          <View style={{ height: 500 }}>
+            {this._renderScrollViewContent(this.props.menu)}
+          </View>
+        </ParallaxScrollView>
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 60,
+            justifyContent: 'space-around',
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 5,
+            flexDirection: 'row'
+          }}
+        >
+          <View
+            style={{
+              textAlign: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+              width: 150
+            }}
+          >
+            <Icon
               name='remove-circle-outline'
               type='MaterialIcons'
               style={{
                 color: 'black',
-                fontSize: 40,
+                fontSize: 40
               }}
-              onPress={()=>this.setState({num:num-1})}
+              onPress={() => this.setState({ num: num - 1 })}
             />
             <Text style={styles.title}>{this.state.num}</Text>
             <Icon
@@ -150,63 +167,64 @@ export default class App extends Component {
               type='MaterialIcons'
               style={{
                 color: 'black',
-                fontSize: 40,
+                fontSize: 40
               }}
-              onPress={()=>this.setState({num:num+1})}
+              onPress={() => this.setState({ num: num + 1 })}
             />
-    </View>
-    <View style={{justifyContent:'space-evenly',
-    flexDirection: 'row'}}>
-        <TouchableOpacity
-          style={styles.SubmitButtonStyle}
-          activeOpacity = { .5 }
-          onPress={()=>{alert('555')}}
-       >
-         <Text style={styles.title2}>ADD TO CART</Text>
-       </TouchableOpacity>
-
-    </View>
-    </View>
+          </View>
+          <View
+            style={{ justifyContent: 'space-evenly', flexDirection: 'row' }}
+          >
+            <TouchableOpacity
+              style={styles.SubmitButtonStyle}
+              activeOpacity={0.5}
+              onPress={() => {
+                alert('555')
+              }}
+            >
+              <Text style={styles.title2}>ADD TO CART</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1
   },
   SubmitButtonStyle: {
-
-    height:45,
-    width:180,
-    backgroundColor:'#00BCD4',
-    borderRadius:10,
+    height: 45,
+    width: 180,
+    backgroundColor: '#00BCD4',
+    borderRadius: 10,
     borderWidth: 1,
     textAlign: 'center',
-    justifyContent:'center',
+    justifyContent: 'center',
     borderColor: '#fff'
   },
   title: {
     color: 'black',
-    fontSize: 16,
+    fontSize: 16
   },
   title2: {
     color: '#fff',
     fontSize: 18,
-    marginHorizontal: 10,
+    marginHorizontal: 10
   },
   scrollViewContent: {
-    top:50,
-    margin:10,
-    backgroundColor: 'white',
+    top: 50,
+    margin: 10,
+    backgroundColor: 'white'
   },
   row: {
     height: 40,
     margin: 16,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between'
   },
   subRow: {
     height: 40,
@@ -214,13 +232,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'flex-start'
   },
-  customize:{
-    flex:1,
-    flexDirection:'column',
-    justifyContent:'flex-start',
-    marginLeft:10,
+  customize: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginLeft: 10
   }
-});
-
+})
